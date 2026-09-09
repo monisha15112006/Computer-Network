6624ce5befd57f09e1562c64679b77807f99bbf3
monisha15112006 <monisha15112006_bcs28@mepcoeng.ac.in>
2026-09-09 15:32:31 +0530

    Create VLab distancevector.c

---
+++ ex4/VLab distancevector.c
@@ -0,0 +1,10 @@
+What is the primary goal of the Distance Vector Routing Algorithm?
+a: To find the shortest path between two nodes
+Which type of information does each node maintain in Distance Vector Routing?
+b: Cost estimates to all other nodes
+How do nodes exchange routing information in Distance Vector Routing?
+b: By exchanging distance vectors with neighbors?
+Which mathematical equation is used to update a node's distance vector?
+b: D(y) = min{c(x, v) + Dᵥ(y)}
+What happens when a node updates its distance vector?
+b: It immediately sends the updated vector to its neighbors
